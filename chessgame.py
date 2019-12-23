5b785ac9fa46ff0aa2bf556040f632386ecdfee7
JacobProvencher <jacob.provencher.1@ulaval.ca>
2019-12-23 13:08:44 -0500

    coup_valide pour pion arranger!

--- chessgame.py
+++ chessgame.py
@@ -155,13 +155,14 @@ class Chess:
 
     def __pieces(self):
 
-        # Mise à jour des coups valides pour chaque fou
+        # Mise à jour des coups valides
         coord = [[position for position in positions.keys()] for positions in self.etat.values()]
-        positions_restantes = {(x, y) for x in range(1, 9) for y in range(1, 9)} - set(coord[0]+ coord[1])
+        positions_restantes = {(x, y) for x in range(1, 9) for y in range(1, 9)} - set(coord[0] + coord[1])
 
         for color, positions in self.etat.items():
             for position, liste in positions.items():
                 x, y = position
+
                 if liste[0] == 'P':
 
                     if color == 'black':
@@ -173,9 +174,11 @@ class Chess:
                                     break
                             self.etat['black'][position][1] = coup_valide
 
+                        elif (x, y-1) in positions_restantes:
+                            self.etat['black'][position][1] = [(x, y-1)]
+
                         else:
-                            if (x, y-1) in positions_restantes:
-                                self.etat['black'][position][1] = [(x, y-1)]
+                            self.etat['black'][position][1] = []
 
                     elif color == 'white':
                         if y == 2:
@@ -186,9 +189,11 @@ class Chess:
                                     break
                             self.etat['white'][position][1] = coup_valide
 
+                        elif (x, y+1) in positions_restantes:
+                            self.etat['white'][position][1] = [(x, y+1)]
+
                         else:
-                            if (x, y+1) in positions_restantes:
-                                self.etat['white'][position][1] = [(x, y+1)]
+                            self.etat['white'][position][1] = []
 
                 else:
                     # Revoir pour simplifier les 'for i in range(1, 9)'
